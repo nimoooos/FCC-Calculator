91c117a7b44a3b36c38d2bdd52384011c4505e03
nimoooos <100112940+nimoooos@users.noreply.github.com>
2024-06-10 16:10:37 -1000

    refactoring

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 let formulaValue = "0";
 let displayValue = "0";
-let inputState = "initialized";
+// let inputState = "initialized";
+const initialState = {
+    isInitialized: true,
+    isDecimal: false,
+    waitingForNewNumber: true
+}
+let inputState = initialState;
 function setInputState(newState){
     /** 
      * states are:
-     * "initialized": no previous input, happens on startup and when pressing CE
+     * "initialized": no previous input, happens on startup and when pressing CE. Cannot take syntax as input
      * "number entry": just entered in a number; can continue accepting numbers, or receive syntax to change state
      * "syntax entry": just entered in a syntax; can accept new syntax to switch, or receive a number to change state
      * 
      * we track this by:
      * "isInitialized" flag: is this initialized? set to true on startup and CE, set to false upon anything else
-     * "canDecimal" flag: can I put a decimal in this number? set to false upon entering a decimal, set to true upon entering a number or initializing
-     * "waitingForNewNumber" flag: if four basic functions or equal sign was used to reset.
+     * "isDecimal" flag: is the current number a decimal number? Set to true on adding decimal, set to false on entering new number
+     * "waitingForNewNumber" flag: set to true if four basic functions or equal sign was used to reset number entry; if already entering numbers, set to false
     */
-    inputState=newState;
+    inputState.isInitialized = newState.isInitialized;
+    inputState.isDecimal = newState.isDecimal;
+    inputState.waitingForNewNumber = newState.waitingForNewNumber;
 }
-let isDecimal = false;
+// let isDecimal = false;
+
+function initialize() {
+    formulaValue="0";
+    displayValue="0";
+    setInputState(initialState);
+}
+
+
 
 $(".key").on("click", function (e) {
+    /**
+     * Called upon keypress, and calls numberHandler or syntaxHandler
+     */
     const KVPair = {
         "zero": 0,
         "one": 1,
@@ -42,6 +61,12 @@ $(".key").on("click", function (e) {
 
     const isNumber = Number(value) === value;
 
+    function updateState(){
+        document.getElementById("formula").innerText=formulaValue;
+        document.getElementById("display").innerText=displayValue;
+        console.log(displayValue,inputState)
+    }
+
     if (isNumber) {
         numberHandler(value);
     } else {
@@ -64,54 +89,41 @@ function numberHandler(value) {
 }
 
 function syntaxHandler(value){
+    function operate(num1, num2, operation) {
+        /*
+        Does the calculation based on input parameters. Should be number, number, and string
+        */
+       switch(operation){
+        case "+":
+            return Number(num1)+Number(num2);
+        case "-":
+            return Number(num1)-Number(num2);
+        case "*":
+            return Number(num1)*Number(num2);
+        case "/":
+            return Number(num1)/Number(num2);
+       }
+    }
     console.log("Syntax key", value, "was pressed.");
     switch(value){
         case "CE":
             initialize();
             break;
         case ".":
-            if(isDecimal){break}
-            formulaValue=formulaValue+".";
-            displayValue=displayValue+".";
-            setInputState("decimal");
-            isDecimal=true;
-            break;
+            console.log("Decimal was pressed. No action taken.")
+            // if(isDecimal){break}
+            // formulaValue=formulaValue+".";
+            // displayValue=displayValue+".";
+            // setInputState("decimal");
+            // isDecimal=true;
+            // break;
         case "=":
             operate(formulaValue, displayValue, "last value") //todo: implement a way to remember last operation used
         default:  // for four functions
             
             formulaValue=displayValue;
             displayValue=operate(formulaValue, displayValue, value);
-            isDecimal=false;
+            // isDecimal=false;
             setInputState("initialized");
     }
-}
-
-function initialize() {
-    formulaValue="0";
-    displayValue="0";
-    setInputState("initialized");
-    isDecimal=false;
-}
-
-function operate(num1, num2, operation) {
-    /*
-    Does the calculation based on input parameters. Should be number, number, and string
-    */
-   switch(operation){
-    case "+":
-        return Number(num1)+Number(num2);
-    case "-":
-        return Number(num1)-Number(num2);
-    case "*":
-        return Number(num1)*Number(num2);
-    case "/":
-        return Number(num1)/Number(num2);
-   }
-}
-
-function updateState(){
-    document.getElementById("formula").innerText=formulaValue;
-    document.getElementById("display").innerText=displayValue;
-    console.log(displayValue,inputState)
 }
--- select_test.js
+++ select_test.js
@@ -1,13 +1,12 @@
-console.log("loading test selector...")
 let shadowRoot = document.getElementById("fcc_test_suite_wrapper").shadowRoot;
-console.log(shadowRoot);
 
 setTimeout(function(){
     let selector = shadowRoot.getElementById("test-suite-selector");
-    console.log(selector);
-    selector.value="javascript-calculator";
+    selector.value = "javascript-calculator";
     let testButton = shadowRoot.getElementById("fcc_test_message-box-rerun-button");
-    testButton.click();
+    setTimeout(function(){
+        testButton.click();
+    },500)
 
     setTimeout(function(){
         let testResultButton = shadowRoot.getElementById("fcc_test_button");
